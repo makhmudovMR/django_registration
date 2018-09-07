707884f43921ab395e366593f37779b02b0a951d
Magomed Makhmudov <mmriadovich@gmail.com>
2018-09-07 20:43:48 +0300

    register

---
+++ djregistration/account/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import RegisterUserForm
+from django.http.response import HttpResponseRedirect, HttpResponse
+
+
+class RegisterUserView(CreateView):
+    form_class = RegisterUserForm
+    template_name = "account/register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect('/')
+        
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        return HttpResponse('User registered')
